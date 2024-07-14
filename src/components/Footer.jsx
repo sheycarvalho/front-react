ae287e0489cc2f67771a6c3283ffcc5b54572f03
Sheyenne Carvalho <94084441+sheycarvalho@users.noreply.github.com>
2024-07-14 14:25:50 -0400

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import styles from '../styles/footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p className={styles.footerText}>Feito com ♡ por Sheyenne Carvalho.</p>
-      <p className={styles.footerText}>Instruída por Simara Conceição da PrograMaria.</p>
+      <p className={styles.footerText}>Feito com ♡ por Sheyenne Carvalho, com mentoria de Simara Conceição para a Programaria.</p>
     </div>
   )
 }
